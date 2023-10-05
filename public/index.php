a5c4cf8bcb8370885cb1709b7f1766da36706eda
bkt059 <bkt.059@gmail.com>
2023-10-05 20:41:02 +0000

    Update code for xml, csv, xls, ods

--- public/index.php
+++ public/index.php
@@ -2,6 +2,8 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
 require __DIR__ . '/../vendor/autoload.php';
 
@@ -83,6 +85,101 @@ $app->get('/xml', function (Request $request, Response $response, $args) {
         ->withHeader('Content-Type', 'application/xml')
         ->withHeader('Content-Disposition', 'attachment; filename="your_file.xml"');
     $response->getBody()->write($xmlOutput);
+
+    return $response;
+});
+
+
+$app->get('/xls', function (Request $request, Response $response, $args) {
+    $url = "https://dilrmp.gov.in/meityDashboard?level=1&scode=32";
+    $options = [
+        "ssl" => [
+            "verify_peer" => false,
+            "verify_peer_name" => false,
+        ],
+        "http" => [
+            "header" => "User-Agent: Mozilla/5.0"
+        ]
+    ];
+    $context = stream_context_create($options);
+    $json_data = file_get_contents($url, false, $context);
+    $data = json_decode($json_data, true);
+
+    $spreadsheet = new Spreadsheet();
+    $sheet = $spreadsheet->getActiveSheet();
+    
+    $rowNumber = 1;
+    foreach ($data as $row) {
+        $columnLetter = 'A';
+        foreach ($row as $cell) {
+            $sheet->setCellValue($columnLetter.$rowNumber, $cell);
+            $columnLetter++;
+        }
+        $rowNumber++;
+    }
+
+    $writer = new Xlsx($spreadsheet);
+
+    // Create a temporary file
+    $temp_file = tempnam(sys_get_temp_dir(), 'phpspreadsheet');
+    $writer->save($temp_file);
+
+    // Read the temporary file into a string
+    $xlsData = file_get_contents($temp_file);
+    unlink($temp_file);  // Delete the temporary file
+
+    // Set the headers and body for the XLSX download
+    $response = $response
+        ->withHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+        ->withHeader('Content-Disposition', 'attachment; filename="your_file.xlsx"');
+    $response->getBody()->write($xlsData);
+
+    return $response;
+});
+
+$app->get('/ods', function (Request $request, Response $response, $args) {
+    $url = "https://dilrmp.gov.in/meityDashboard?level=1&scode=32";
+    $options = [
+        "ssl" => [
+            "verify_peer" => false,
+            "verify_peer_name" => false,
+        ],
+        "http" => [
+            "header" => "User-Agent: Mozilla/5.0"
+        ]
+    ];
+    $context = stream_context_create($options);
+    $json_data = file_get_contents($url, false, $context);
+    $data = json_decode($json_data, true);
+
+    $spreadsheet = new Spreadsheet();
+    $sheet = $spreadsheet->getActiveSheet();
+    
+    $rowNumber = 1;
+    foreach ($data as $row) {
+        $columnLetter = 'A';
+        foreach ($row as $cell) {
+            $sheet->setCellValue($columnLetter.$rowNumber, $cell);
+            $columnLetter++;
+        }
+        $rowNumber++;
+    }
+
+    $writer = new Ods($spreadsheet);
+
+    // Create a temporary file
+    $temp_file = tempnam(sys_get_temp_dir(), 'phpspreadsheet');
+    $writer->save($temp_file);
+
+    // Read the temporary file into a string
+    $odsData = file_get_contents($temp_file);
+    unlink($temp_file);  // Delete the temporary file
+
+    // Set the headers and body for the ODS download
+    $response = $response
+        ->withHeader('Content-Type', 'application/vnd.oasis.opendocument.spreadsheet')
+        ->withHeader('Content-Disposition', 'attachment; filename="your_file.ods"');
+    $response->getBody()->write($odsData);
 
     return $response;
 });
